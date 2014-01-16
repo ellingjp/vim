ba588f9e56930d5ee3211a0ee99e403fd4a5b51e
Jonathan Ellington <ellingjp@uw.edu>
2014-01-16 13:23:56 -0800

    Added no admin curl install.  Currently not working.  curl.cmd calls cur.exe which isn't found. Related --del-path doesn't seem to work without admin priv.

--- setup.py
+++ setup.py
@@ -14,12 +14,24 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-v", "--verbose", help="specify verbosity", 
                     action="store_true")
 parser.add_argument("--vim-dir", help="specify vimfiles directory")
-parser.add_argument("--git-dir", help="(windows only) specify where to look                     for git files (ex C:\Program Files\Git)")
+parser.add_argument("--git-dir", help="(windows only) specify where to look\
+                    for git files (ex C:\Program Files\Git)")
 parser.add_argument("--vimrc-path", help="specify vimrc path", default="~")
 parser.add_argument("--vimrc-copy", choices=["sym", "copy", "none"], 
-                    default="sym", help="specify how to copy the vimrc to                       vimdir (symlink, hard copy [overwrites any existing file                    !], or none)")
+                    default="sym", help="specify how to copy the vimrc to\
+                    vimdir (symlink, hard copy [overwrites any existing file\
+                    !], or none)")
 parser.add_argument("--no-curl", action="store_true",
                     help="(windows only) don't try to install curl")
+parser.add_argument("--no-admin", action="store_true",
+                    help="try to install curl with no admin priviledges.\
+                          warning: this will create a user PATH variable\
+                          which will hide the system PATH variable.\
+                          thus, changes to the system PATH will not be\
+                          reflected.  run with --del-path to fix.")
+parser.add_argument("--del-path", action="store_true",
+                    help="(windows only) removes user-local PATH variable\
+                          created with --no-admin.")
 parser.add_argument("--no-vundle", action="store_true", 
                     help="don't try to clone vundle")
 args = parser.parse_args()
@@ -27,39 +39,53 @@ args = parser.parse_args()
 # constants
 vundlerepo = "https://github.com/gmarik/vundle.git"
 windows = "nt"
-curlfile = os.path.join(os.path.dirname(os.path.abspath(__file__)), "curl.cmd")
-vimrc = os.path.join(os.path.dirname(os.path.abspath(__file__)), "vimrc")
+setupdir = os.path.dirname(os.path.abspath(__file__))
+curlfile = os.path.join(setupdir, "curl.cmd")
+vimrc = os.path.join(setupdir, "vimrc")
 
 system = os.name
 home = os.path.expanduser('~')
 
+# see if we should delete user local PATH
+if (args.del_path and system == windows):
+    subprocess.call(['setx', 'PATH', '""', '-m'])
+    sys.exit()
+elif (args.del_path and system != windows):
+    print("Error: --no-admin called on non-windows platform, quitting")
+    sys.exit()
+    
 # If running windows, try to copy curl.cmd into the git binaries folder
 if (args.no_curl):
     if args.verbose: print("--no-curl specified.  Skipping curl install.")
 elif (system != windows):
     if args.verbose: print("Not windows.  Skipping curl install.")
 elif (system == windows):
-    print("Trying to install curl.cmd...")
-    if (args.git_dir):
-        gdir = args.git_dir + "/cmd"
-    elif os.path.isdir("C:\Program Files\Git"):
-        gdir = "C:\Program Files\Git"
-    elif os.path.isdir("C:\Program Files (x86)\Git"):
-        gdir = "C:\Program Files (x86)\Git"
+    if (args.no_admin):
+        print("Trying to install curl.cmd locally, with no admin priviledges...")
+        subprocess.call(['setx', 'PATH', '%PATH%;'+setupdir])
+        
     else:
-        print("Could not find git installation directory!")
-    
-    if args.verbose:
-        print("Attempting to copy curl.cmd to", gdir)
+        print("Trying to install curl.cmd...")
+        if (args.git_dir):
+            gdir = args.git_dir + "/cmd"
+        elif os.path.isdir("C:\Program Files\Git"):
+            gdir = "C:\Program Files\Git"
+        elif os.path.isdir("C:\Program Files (x86)\Git"):
+            gdir = "C:\Program Files (x86)\Git"
+        else:
+            print("Could not find git installation directory!")
+        
+        if args.verbose:
+            print("Attempting to copy curl.cmd to", gdir)
 
-    try:
-        shutil.copyfile(curlfile, gdir)
-    except PermissionError as err:
-        warning("curl not installed, insufficient permissions. try running as admin, or install manually.")
-    except IOError as err:
-        warning("curl not installed, io error:",err)
-    else:
-        print("curl successfully installed.")
+        try:
+            shutil.copyfile(curlfile, gdir)
+        except PermissionError as err:
+            warning("curl not installed, insufficient permissions. try running as admin, or install manually.")
+        except IOError as err:
+            warning("curl not installed, io error:",err)
+        else:
+            print("curl successfully installed.")
 
 print()
 
